318c0688abc08cda4de0b2dc9c10e53022963158
JIMIN SEO <66631295+JIMIN-iOSDev@users.noreply.github.com>
2024-11-11 19:40:08 +0900

    [level 1] Title: 자릿수 더하기, Time: 0.06 ms, Memory: 16.1 MB -BaekjoonHub

--- 프로그래머스/1/12931. 자릿수 더하기/자릿수 더하기.swift
+++ 프로그래머스/1/12931. 자릿수 더하기/자릿수 더하기.swift
@@ -3,11 +3,9 @@ import Foundation
 func solution(_ n:Int) -> Int
 {
     var answer:Int = 0
-    var random: Int = n
     
-    while random > 0 { 
-        answer += (random % 10)
-        random /= 10
+    for i in String(n){
+        answer += Int(String(i))!
     }
     return answer
 }
