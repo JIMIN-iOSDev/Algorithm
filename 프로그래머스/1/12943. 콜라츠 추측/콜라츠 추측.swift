264cda58fb15013610ad6202ea63ed4c56f3c5e9
JIMIN SEO <66631295+JIMIN-iOSDev@users.noreply.github.com>
2024-11-20 09:54:27 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.02 ms, Memory: 16.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.swift
@@ -0,0 +1,18 @@
+func solution(_ num:Int) -> Int {
+    var count = 0
+    var n = num
+    
+    while n > 1 {
+        if count == 501 {
+            return -1
+        }
+        if n % 2 == 0 {
+            n /= 2
+        } else {
+            n = (n * 3) + 1
+        }
+        count += 1
+    }
+    
+    return count
+}
